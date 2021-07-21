20d5d63c639b380e895e688a3c114c51021e69c0
Adam BENJBARA <benjbara13@gmail.com>
2021-07-21 06:04:57 -0700

    sould_write_foo_when_the_number_is_devisible_by_3:test ko

--- Readme.md
+++ Readme.md
@@ -3,14 +3,13 @@
 ## Statement:
 This project implement a function String compute(String) which implements the following rules.
 
-###Step 1
-Rules:
+Step 1:  
 If the number is divisible by 3, write “Foo” instead of the number
 If the number is divisible by 5, add “Bar”
 If the number is divisible by 7, add “Qix”
 For each digit 3, 5, 7, add “Foo”, “Bar”, “Qix” in the digits order.
 
-###Step 2
+Step 2:  
 We have a new business request: we must keep a trace of 0 in numbers, each 0 must be replaced by char “*“.
 
 
--- src/test/java/foo/bar/qix/wemanity/ComputeServiceTest.java
+++ src/test/java/foo/bar/qix/wemanity/ComputeServiceTest.java
@@ -1,8 +1,29 @@
 package foo.bar.qix.wemanity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import foo.bar.qix.wemanity.service.ComputeService;
 
 @SpringBootTest
 public class ComputeServiceTest {
+	
+	@Autowired
+	private ComputeService computeService;
+	
+	@Test
+	public void sould_write_the_same_number_when_its_not_devisible_by_3_5_or_7(){
+		// given
+		String input = "1";
+		
+		// when
+		String output = computeService.compute(input);
+		
+		//then
+		assertThat(output).isEqualTo(input);		
+	}
 
 }
