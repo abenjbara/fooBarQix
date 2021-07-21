a13d979526b8a9e532d4ce62381c78a97ea16312
Adam BENJBARA <benjbara13@gmail.com>
2021-07-21 06:34:53 -0700

    sould_write_foo_when_the_number_is_devisible_by_3: test ok

--- src/main/java/foo/bar/qix/wemanity/service/ComputeServiceImpl.java
+++ src/main/java/foo/bar/qix/wemanity/service/ComputeServiceImpl.java
@@ -5,9 +5,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class ComputeServiceImpl implements ComputeService {
 
+	private static final int ZERO = 0;
+
 	@Override
 	public String compute(String input) {
-		return null;
+		StringBuilder output = new StringBuilder();
+		int number = Integer.valueOf(input);
+		
+		if(number % 3 == ZERO) {
+			output.append("Foo");
+		}
+		
+		return output.toString();
 	}
 
 }
